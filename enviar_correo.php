47602e639b4b8b6e169afac83c7a662ab468d61c
jdiaz14 <93689352+jdiaz14@users.noreply.github.com>
2023-03-18 13:32:35 -0500

    ahora si?

--- enviar_correo.php
+++ enviar_correo.php
@@ -1,45 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Envío de correo electrónico</title>
-</head>
-<body>
-    <?php
-    // Cargar PHPMailer
-    require 'vendor/autoload.php';
+<?php
+// Cargar PHPMailer
+require 'vendor/autoload.php';
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    // Crear una nueva instancia de PHPMailer
-    $mail = new PHPMailer(true);
+// Crear una nueva instancia de PHPMailer
+$mail = new PHPMailer(true);
 
-    try {
-        // Configurar el servidor SMTP de Mailtrap
-        $mail->isSMTP();
-        $mail->Host = 'smtp.mailtrap.io';
-        $mail->SMTPAuth = true;
-        $mail->Username = '520ea93e2a1086:225633920252f6';
-        $mail->Password = 'Julibdm811';
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 2525;
+try {
+    // C servidor SMTP de Mailtrap
+    $mail->isSMTP();
+    $mail->Host = 'smtp.mailtrap.io';
+    $mail->SMTPAuth = true;
+    $mail->Username = '520ea93e2a1086:225633920252f6';
+    $mail->Password = 'Julibdm811';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 2525;
 
-        // Configurar el correo electrónico
-        $mail->setFrom('juli.andgu16@gmail.com', 'Julian Diaz');
-        $mail->addAddress('jujudidiaz@gmail.com', 'Andres Guevara');
-        $mail->Subject = 'Asunto del correo electrónico';
-        $mail->Body = 'Cuerpo del correo electrónico';
+    //  correo electrónico
+    $mail->setFrom($_POST['email'], $_POST['nombre']);
+    $mail->addAddress('destinatario@example.com', 'Destinatario');
+    $mail->Subject = 'Nuevo mensaje del formulario de contacto';
+    $mail->Body = 'Nombre: ' . $_POST['nombre'] . '<br>' .
+                  'Email: ' . $_POST['email'] . '<br>' .
+                  'Mensaje: ' . $_POST['mensaje'];
 
-    
-
-        // Enviar el correo electrónico
-        $mail->send();
-        echo 'El correo electrónico se envió correctamente';
-    } catch (Exception $e) {
-        echo 'No se pudo enviar el correo electrónico. Error: ' . $mail->ErrorInfo;
-    }
-    ?>
-</body>
-</html>
+    // Enviar el correo electrónico
+    $mail->send();
+    echo 'El correo electrónico se envió correctamente';
+} catch (Exception $e) {
+    echo 'No se pudo enviar el correo electrónico. Error: ' . $mail->ErrorInfo;
+}
+?>
